e4679d47c11a557f06984f73f196613aa9f17ef5
MRBOX <olamigokeadebayo90@gmail.com>
2024-10-17 08:30:48 +0100

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <h1>FOP</h1>
-      </div>
+    <div className='flex w-full justify-between items-center px-4 py-4'>
+        <div>
+        <h1 className='font-bold'>FOP</h1>
+        </div>
+
+        <div className='flex gap-2 justify-center items-center'>
+            <FaWhatsapp  size={30}/>
+            <FaInstagram  size={30}/>
+        </div>
+
+    
     </div>
   )
 }
