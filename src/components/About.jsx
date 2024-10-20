655bbd21fa066b9ec4f4f091087ac22c270e0887
MRBOX <olamigokeadebayo90@gmail.com>
2024-10-20 23:49:53 +0100

    ABout jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import HeroImg from "../assets/images/bible.jpeg"
 
 const About = () => {
   return (
     <div className='w-[1240px] mx-auto my-9'>
-      <div>
-        <h2>About</h2>
+      <div className='my-5'>
+        <h2 className='font-bold text-3xl'>About</h2>
       </div>
 
-      <div>
-        
+    <div className='flex gap-4'>
+      <div className='flex w-[600px]'>
+        <img src={HeroImg} alt="" srcset="" className='w-[120px] rounded-md' />
+
+        <div className='mx-3'>
+            <h2 className='font-extrabold'>OUR FELLOWSHIP</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quasi, illo magni. Nisi illo omnis, eveniet nostrum soluta distinctio suscipit velit.</p>
+        </div>
+      </div>
+
+      <div className='flex w-[600px]'>
+        <img src={HeroImg} alt="" srcset="" className='w-[120px] rounded-md' />
+
+        <div className='mx-3'>
+            <h2 className='font-extrabold'>OUR PURPOSE</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quasi, illo magni. Nisi illo omnis, eveniet nostrum soluta distinctio suscipit velit.</p>
+        </div>
+      </div>
       </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='flex w-[1240px] mx-auto  items-start mt-5 gap-20 h-vh'>
       <div className='w-[500px]'>
-        <h1 className='mb-16 font-extrabold'>WELCOME TO FLAMES OF POWER.</h1>
+        <h1 className='mb-10 font-extrabold text-6xl'>WELCOME TO FLAMES OF POWER.</h1>
 
         <div className='flex gap-5'>
         <div className='bg-black w-9'></div>
@@ -13,8 +13,10 @@ const Hero = () => {
             eligendi saepe amet esse ut, ea voluptatem debitis aperiam eos asperiores, ad ipsam officia similique consectetu
             eligendi saepe amet esse ut, ea voluptatem debitis aperiam eos asperiores, ad ipsam officia similique consectetu
             </p>
-            </div>
 
+            
+            </div>
+            <p className='text-right text-slate-600 font-semibold'>Roman 5:12</p>
       </div>
 
       <div>
