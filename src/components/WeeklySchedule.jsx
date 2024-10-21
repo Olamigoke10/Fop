22895553e86eb7a2c9516a245fbd6f03c396e1f8
MRBOX <olamigokeadebayo90@gmail.com>
2024-10-21 03:01:55 +0100

    Adding responsiveness

--- src/components/WeeklySchedule.jsx
+++ src/components/WeeklySchedule.jsx
@@ -5,10 +5,20 @@ const WeeklySchedule = () => {
     <div className='bg-stone-200 w-full p-6'>
         <h2 className='py-3'>WEEKLY SCHEDULE</h2>
 
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center md:flex-row md:justify-start'>
         <div className='bg-gray-600 p-5 w-[300px] rounded-md mx-auto text-center'>
             <div className='bg-white rounded-md p-3 mx-2 mb-3'>
                 <h2>WED</h2>
+                <p>1h</p>
+            </div>
+            <div className='text-center'>
+                <h1>Prayers & Bible Study</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, asperiores.</p>
+            </div>
+        </div>
+        <div className='bg-gray-600 p-5 w-[300px] rounded-md mx-auto mt-3'>
+            <div className='bg-white rounded-md p-3 mx-2 mb-3 text-center'>
+                <h2>SAT</h2>
                 <p>1h</p>
             </div>
             <div className='text-center'>
